839afb03621a1f868121d068dfe3d67976c56939
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2025-02-21 23:45:53 +0000

    changing hidden size to 500 to fix bug

--- app/torch_utils.py
+++ app/torch_utils.py
@@ -21,7 +21,7 @@ class NeuralNet(nn.Module):
         return out
 
 input_size = 784  # 28x28
-hidden_size = 100
+hidden_size = 500
 num_classes = 10
 model = NeuralNet(input_size, hidden_size, num_classes)
 # device = torch.device("cpu")
